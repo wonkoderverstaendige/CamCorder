8d3c107be09ec3da4616d761ce41b227f75cc83a
Ronny Eichler <R.Eichler@science.ru.nl>
2018-09-23 16:46:43 +0200

    Shared array for most recent tracking/display frame

--- camcorder/hextrack.py
+++ camcorder/hextrack.py
@@ -1,9 +1,12 @@
+import ctypes
 import cv2
 import threading
 from queue import Queue, Empty, Full
 import time
 import logging
 from pathlib import Path
+import multiprocessing as mp
+import numpy as np
 
 from camcorder.lib.framesources import Frame
 
@@ -15,7 +18,12 @@ N_FRAMES_FPS_WINDOW = 10
 
 FRAME_WIDTH = 800
 FRAME_HEIGHT = 600
+FRAME_N_BYTES = FRAME_HEIGHT * FRAME_WIDTH * 3
+shared_arr = mp.Array(ctypes.c_ubyte, FRAME_N_BYTES)
 
+def buf_to_numpy(buf, shape):
+    """Return numpy object from a raw buffer, e.g. multiprocessing Array"""
+    return np.frombuffer(buf.get_obj(), dtype=np.ubyte).reshape(shape)
 
 class Grabber(threading.Thread):
     def __init__(self, aux_queue, out_queue, trigger_event):  # , in_queue, out_queue
@@ -26,13 +34,20 @@ class Grabber(threading.Thread):
         self.capture = None
         self.frame = None
 
+        self.width = FRAME_WIDTH
+        self.height = FRAME_HEIGHT
+
         self._aux_queue = aux_queue
+        global shared_arr
+        with shared_arr.get_lock():
+            self._shared_arr = shared_arr
+            logging.debug(shared_arr)
+            self._fresh_frame = buf_to_numpy(shared_arr, (FRAME_HEIGHT, FRAME_WIDTH, -1))
+            logging.debug(hex(self._fresh_frame.ctypes.data))
+
         self._write_queue = out_queue
         self._trigger = trigger_event
         self._avg_fps = 15
-
-        self.width = FRAME_WIDTH
-        self.height = FRAME_HEIGHT
 
         logging.debug('Initialized')
 
@@ -42,6 +57,9 @@ class Grabber(threading.Thread):
 
         self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, self.width)
         self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
+
+        # MAXIMUM fps. If light conditions drop, it may drop the frame rate no matter what you ask.
+        self.capture.set(cv2.CAP_PROP_FPS, 20.)
 
         t0 = cv2.getTickCount()
 
@@ -58,22 +76,27 @@ class Grabber(threading.Thread):
                                                                                  elapsed, self._avg_fps))
             self.n_frames += 1
 
-            self.relay()
+            self.relay_frames()
 
             t0 = cv2.getTickCount()
 
         logging.debug('Stopping loop in {}!'.format(self.name))
 
-    def relay(self):
-        try:
-            self._aux_queue.put(self.frame, timeout=.5)
-        except Full:
-            pass
-
+    def relay_frames(self):
+        # Forward frame to writer
         try:
             self._write_queue.put(self.frame, timeout=.5)
         except Full:
-            print('Dropped frame {}'.format(self.frame.index))
+            logging.warning('Dropped frame {}'.format(self.frame.index))
+
+        # Forward frame for tracking and display
+        # try:
+        #     self._aux_queue.put(self.frame, timeout=.5)
+        # except Full:
+        #     pass
+        # NOTE: [:] indicates to reuse the buffer
+        with self._shared_arr.get_lock():
+            self._fresh_frame[:] = self.frame.img
 
 
 class Writer(threading.Thread):
@@ -138,6 +161,10 @@ class HexTrack:
                                trigger_event=self.ev_kill_grabber)
         self.grabber.start()
         self.frame = None
+        global shared_arr
+        with shared_arr.get_lock():
+            self._shared_arr = shared_arr
+            logging.debug(repr(self._shared_arr))
 
         self.writer = Writer(in_queue=self.write_queue, ev_alive=self.ev_kill_writer, ev_recording=self.ev_rec_writer)
         self.writer.start()
@@ -147,12 +174,14 @@ class HexTrack:
     def run(self):
         while self.grabber.is_alive():
             try:
-                self.frame = self.frame_queue.get(timeout=.5)
+                # self.frame = self.frame_queue.get(timeout=.5)
+                with self._shared_arr.get_lock():
+                    self.frame = buf_to_numpy(self._shared_arr, (FRAME_HEIGHT, FRAME_WIDTH, 3))
             except Empty:
                 continue
 
             if self.frame is not None:
-                cv2.imshow('frame', self.frame.img)
+                cv2.imshow('frame', self.frame)
                 key = cv2.waitKey(1)
                 if key == ord('q'):
                     self.ev_kill_grabber.set()
@@ -177,5 +206,6 @@ class HexTrack:
 
 
 if __name__ == '__main__':
+    mp.freeze_support()
     ht = HexTrack()
     ht.run()
