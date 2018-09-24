76a79011eea0af4cf801da5396da8d876716f735
Ronny Eichler <R.Eichler@science.ru.nl>
2018-09-24 14:41:22 +0200

    List of potential codecs/containers

---
+++ camcorder/util/codecs.py
@@ -0,0 +1,31 @@
+fourcc_list = [
+    'XVID',
+    '3IVD',
+    'CJPG',
+    'DIV4',
+    'DIV5',
+    'DIVX',
+    'DX50',
+    'FFV1',
+    'FMP4',
+    'FRWD',
+    'HEVC',
+    'HFYU',
+    'LMP4',
+    'MJ2C',
+    'MJPG',
+    'MP42',
+    'MP4V',
+    'MPEG',
+    'MPG4',
+    'PIMJ',
+    'XVID',
+    'X264'
+]
+
+container_list = [
+    'avi',
+    'mp4',
+    'mkv',
+    'wmv'
+]
